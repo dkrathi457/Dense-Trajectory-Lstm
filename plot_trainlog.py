01ade9e26f4c40e16a10cd480ee22c698fe76259
cis <rathi.deepak@live.com>
2017-07-26 15:51:49 -0400

    Adding plot

---
+++ plot_trainlog.py
@@ -0,0 +1,27 @@
+"""
+Given a training log file, plot something.
+"""
+import csv
+import matplotlib.pyplot as plt
+
+def main(training_log):
+    with open(training_log) as fin:
+        reader = csv.reader(fin)
+        next(reader, None)  # skip the header
+        accuracies = []
+        top_5_accuracies = []
+        cnn_benchmark = []  # this is ridiculous
+        for epoch,acc,loss,val_acc,val_loss, in reader:
+            accuracies.append(float(val_acc))
+            top_5_accuracies.append(float(acc))
+            cnn_benchmark.append(0.65)  # ridiculous
+
+        plt.plot(accuracies, label = "Validation Accuracy")
+        plt.plot(top_5_accuracies ,label = "Training Accuracy")
+        plt.plot(cnn_benchmark, label= "Predefine Benchmark")
+        plt.legend(loc = "bottom right")
+        plt.show()
+
+if __name__ == '__main__':
+    training_log = 'data/logs/-training-dt1501093474.7.log'
+    main(training_log)
